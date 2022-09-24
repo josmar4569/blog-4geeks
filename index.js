9a7b7c47bcb9aaeb4b1dfbc4bb1327b21a995f90
Josmar Manrique <manriquejosmar64@gmail.com>
2022-09-24 10:29:30 -0400

    Agregando usuarios

---
+++ index.js
@@ -0,0 +1,28 @@
+const usuarioJorge = ["Jtutortecnico"]
+const contraseñaJorge = [1234] 
+const usuarioDavid = ["Dtutoreconomico"]
+const contraseñaDavid =[2345]
+const usuarioDomingo = []
+const contraseñaDomingo = [3456]
+const usuarioHector = ["Patrocinador"]
+const contraseñaHector = [4567]
+
+function usuarios(){
+    if(usuarioJorge == "Jtutortecnico" && contraseñaJorge == 1234){
+
+    }
+    else if(usuarioDavid == "Dtutoreconomico" && contraseñaDavid == 2345){
+
+    }
+    else if (usuarioDomingo == "" && contraseñaDomingo == 3456){
+
+    }
+    else if(usuarioHector == "Patrocinador" && contraseñaHector == 4567){
+
+    }
+    else{
+        alert("Por favor, verifique ")
+    }
+    return usuarios
+}
+console.log()
